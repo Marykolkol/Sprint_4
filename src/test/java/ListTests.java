64cc32a4067d83eadb2efe17ae33cf427722359b
Mariya <kolomina.mariya@wb.ru>
2024-12-30 03:18:04 +0300

    done task

--- src/test/java/ListTests.java
+++ src/test/java/ListTests.java
@@ -1,13 +1,10 @@
 import Pages.MainPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 
--- src/test/java/OrderTests.java
+++ src/test/java/OrderTests.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertTrue;
 
